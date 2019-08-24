1ccb8c8e67012cde7c82fdee42960dbeed2d83f9
Gerald Aburto Urroz <geraldaburto@pop-os.localdomain>
2019-08-24 13:26:19 -0600

    Added new Header component.

---
+++ src/components/header/header.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../logo.svg';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/tienda'>TIENDA</Link>
+            <Link className='option' to='/contacto'>CONTACTO</Link>
+        </div>
+    </div>
+);
+
+export default Header;
