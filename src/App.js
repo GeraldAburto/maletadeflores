a4fdfab53af32b2cb9177cf3fedf1f1177c340ac
Gerald Aburto Urroz <geraldaburto@pop-os.localdomain>
2019-08-29 20:10:30 -0600

    Added firebase functionality to save user data.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import 'flexboxgrid/dist/flexboxgrid.css';
 import './App.css';
@@ -23,8 +23,20 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        })
+      }
+      else
+        this.setState({ currentUser: userAuth });
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/firestore';
-import 'firebase/auth'; 
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyD1u6fSbh7EeZA2l3wxBnmIweCPKkfa6vU",
@@ -25,5 +25,30 @@ const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 googleAuthProvider.setCustomParameters({ prompt: 'select_account' });
 
 export const signInWithGoogle = () => auth.signInWithPopup(googleAuthProvider);
+
+/* Store current user in database */
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    let userRef = firestore.doc(`users/${userAuth.uid}`);
+    let userSnapShot = await userRef.get();
+
+    if (!userSnapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.error('error creting user', error.message);
+        }
+    }
+
+    return userRef;
+}
 
 export default firebase;
